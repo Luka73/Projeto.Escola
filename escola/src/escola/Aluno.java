7aff72957e8535fc7c695f0ddd1bd7d8d016d551
Luana <luanafernandes73@gmail.com>
2019-09-12 21:56:32 -0300

    Creating Aluno

---
+++ escola/src/escola/Aluno.java
@@ -0,0 +1,8 @@
+package escola;
+
+public class Aluno {
+	String nome;
+	int idade;
+	double nota1;
+	double nota2;
+}
---
+++ escola/src/escola/CriarAluno.java
@@ -0,0 +1,15 @@
+package escola;
+
+public class CriarAluno {
+
+	public static void main(String[] args) {
+		
+		Aluno aluno1 = new Aluno();
+		aluno1.nome = "Juliana";
+		aluno1.idade = 34;
+		aluno1.nota1 = 7.5;
+		aluno1.nota2 = 8.2;
+
+	}
+
+}
